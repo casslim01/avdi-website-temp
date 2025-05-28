7e42dbbc1ac3186259436a5b0f6e8e0dcc3c754c
Cassler <cashewmlg420@gmail.com>
2025-05-28 13:14:22 +0800

    new router

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 // import App from './App.tsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
 import Homepage from './pages/Homepage.tsx'
 import Showroom from './pages/Showroom.tsx'
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
     element: <Homepage />,
